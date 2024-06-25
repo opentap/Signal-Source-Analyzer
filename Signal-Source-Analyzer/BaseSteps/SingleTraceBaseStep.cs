b0d51b5090d0d762de5898fba14e453d01876760
Alan Copeland <alan_copeland@keysight.com>
2024-06-25 16:24:47 -0600

    Cleanup unneeded using statement

--- Signal-Source-Analyzer/BaseSteps/SingleTraceBaseStep.cs
+++ Signal-Source-Analyzer/BaseSteps/SingleTraceBaseStep.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.Remoting.Channels;
 using System.Text;
 
 namespace Signal_Source_Analyzer
