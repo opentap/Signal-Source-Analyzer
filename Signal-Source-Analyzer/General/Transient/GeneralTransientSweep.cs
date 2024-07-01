79cfd3a84c196c9a58a1f088a1a84c4c632da38b
Carlos Montes <carlos.montes@keysight.com>
2024-07-01 17:31:48 -0500

    Adding Transient Sweep

---
+++ Signal-Source-Analyzer/General/Transient/GeneralTransientSweep.cs
@@ -0,0 +1,183 @@
+﻿using OpenTap;
+using OpenTap.Plugins.PNAX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signal_Source_Analyzer
+{
+
+    [Display("Sweep", Groups: new[] { "Signal Source Analyzer", "General", "Transient" }, Description: "Insert a description here")]
+    public class GeneralTransientSweep : SSAXBaseStep
+    {
+        #region Settings
+
+        [Display("Sweep Type", Group: "Settings", Order: 20.01, Description: "Sets and read the transition mode, Wide-Narrow or Narrow-Narrow.")]
+        public Transient_SweepTypeEnum SweepType { get; set; }
+
+        [Display("Time Coupling", Group: "Settings", Order: 20.02, Description: "Enable and disable the time coupling for two measurements (Wide-Narrow or Narrow1-Narrow2")]
+        public bool TimeCoupling { get; set; }
+
+        [Display("Number Of Points", Group: "Settings", Order: 20.03, Description: "Sets the number of data points for the measurement.")]
+        public int NumberOfPoints { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.WN, HideIfDisabled = true)]
+        [Display("Frequency Range", Group: "Wide Mode", Order: 30.01, Description: "Set and read the frequency range for wide band.")]
+        public Transient_FrequencyRangeEnum FrequencyRange { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.WN, HideIfDisabled = true)]
+        [Display("Wide Time Span", Group: "Wide Mode", Order: 30.02, Description: "Set and read the time span of wide band.")]
+        [Unit("S", UseEngineeringPrefix: true, StringFormat: "0.000")]
+        public double WideTimeSpan { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.WN, HideIfDisabled = true)]
+        [Display("Wide Time Offset", Group: "Wide Mode", Order: 30.03, Description: "Set and read the time offset for wide band.")]
+        [Unit("S", UseEngineeringPrefix: true, StringFormat: "")]
+        public double WideTimeOffset { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.WN, HideIfDisabled = true)]
+        [Display("Wide Reference", Group: "Wide Mode", Order: 30.04, Description: "Set and read the position at the reference point of wide band.")]
+        public Transient_WideReferenceEnum WideReference { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.WN, HideIfDisabled = true)]
+        [Display("Wide VBW", Group: "Wide Mode", Order: 30.05, Description: "Sets and read the Video Band Width (VBW")]
+        [Unit("Hz", UseEngineeringPrefix: true, StringFormat: "0.000000")]
+        public double WideVBW { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.WN, HideIfDisabled = true)]
+        [Display("Wide VBW auto", Group: "Wide Mode", Order: 30.06, Description: "Sets and read the Video Band Width (VBW")]
+        public bool WideVBWauto { get; set; }
+
+        [Display("Center Frequency", Group: "Narrow1", Order: 40.01, Description: "Set and read the center frequency for Narrow1 or Narrow2.")]
+        [Unit("Hz", UseEngineeringPrefix: true, StringFormat: "0.000000000")]
+        public double Narrow1CenterFrequency { get; set; }
+
+        [Display("Frequency Span", Group: "Narrow1", Order: 40.02, Description: "Set and read the frequency span of narrow band 1/2.")]
+        public Transient_FrequencySpanEnum Narrow1FrequencySpan { get; set; }
+
+        [Display("Narrow Time Span", Group: "Narrow1", Order: 40.03, Description: "Set and read the time span of the narrow band 1 or 2.")]
+        [Unit("S", UseEngineeringPrefix: true, StringFormat: "0.000")]
+        public double Narrow1TimeSpan { get; set; }
+
+        [Display("Narrow Time Offset", Group: "Narrow1", Order: 40.04, Description: "Set and read the offset time at the reference point for the narrow band 1 or 2.")]
+        [Unit("S", UseEngineeringPrefix: true, StringFormat: "0")]
+        public double Narrow1TimeOffset { get; set; }
+
+        [Display("Narrow Reference", Group: "Narrow1", Order: 40.05, Description: "Set and read the position at the reference point of the narrow band 1 or 2.")]
+        public Transient_NarrowReferenceEnum Narrow1Reference { get; set; }
+
+        [Display("Narrow VBW", Group: "Narrow1", Order: 40.06, Description: "Sets and read the Video Band Width (VBW")]
+        [Unit("Hz", UseEngineeringPrefix: true, StringFormat: "0.000000")]
+        public double Narrow1VBW { get; set; }
+
+        [Display("Narrow VBW auto", Group: "Narrow1", Order: 40.07, Description: "Sets and read the Video Band Width (VBW")]
+        public bool Narrow1VBWauto { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Center Frequency", Group: "Narrow2", Order: 50.01, Description: "Set and read the center frequency for Narrow1 or Narrow2.")]
+        [Unit("Hz", UseEngineeringPrefix: true, StringFormat: "0.000000000")]
+        public double Narrow2CenterFrequency { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Frequency Span", Group: "Narrow2", Order: 50.02, Description: "Set and read the frequency span of narrow band 1/2.")]
+        public Transient_FrequencySpanEnum Narrow2FrequencySpan { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Narrow Time Span", Group: "Narrow2", Order: 50.03, Description: "Set and read the time span of the narrow band 1 or 2.")]
+        [Unit("S", UseEngineeringPrefix: true, StringFormat: "0.000")]
+        public double Narrow2TimeSpan { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Narrow Time Offset", Group: "Narrow2", Order: 50.04, Description: "Set and read the offset time at the reference point for the narrow band 1 or 2.")]
+        [Unit("S", UseEngineeringPrefix: true, StringFormat: "0")]
+        public double Narrow2TimeOffset { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Narrow Reference", Group: "Narrow2", Order: 50.05, Description: "Set and read the position at the reference point of the narrow band 1 or 2.")]
+        public Transient_NarrowReferenceEnum Narrow2Reference { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Narrow VBW", Group: "Narrow2", Order: 50.06, Description: "Sets and read the Video Band Width (VBW")]
+        [Unit("Hz", UseEngineeringPrefix: true, StringFormat: "0.000000")]
+        public double Narrow2VBW { get; set; }
+
+        [EnabledIf("SweepType", Transient_SweepTypeEnum.NN, HideIfDisabled = true)]
+        [Display("Narrow VBW auto", Group: "Narrow2", Order: 50.07, Description: "Sets and read the Video Band Width (VBW")]
+        public bool Narrow2VBWauto { get; set; }
+
+
+        #endregion
+
+        public GeneralTransientSweep()
+        {
+            SweepType = Transient_SweepTypeEnum.WN;
+            TimeCoupling = false;
+            NumberOfPoints = 201;
+
+            FrequencyRange = Transient_FrequencyRangeEnum.R8G;
+            WideTimeSpan = 0.1;
+            WideTimeOffset = 0;
+            WideReference = Transient_WideReferenceEnum.CENTer;
+            WideVBW = 2000;
+            WideVBWauto = true;
+
+            Narrow1CenterFrequency = 1000000000;
+            Narrow1FrequencySpan = Transient_FrequencySpanEnum.R30;
+            Narrow1TimeSpan = 0.1;
+            Narrow1TimeOffset = 0;
+            Narrow1Reference = Transient_NarrowReferenceEnum.CENTer;
+            Narrow1VBW = 2000;
+            Narrow1VBWauto = true;
+
+            Narrow2CenterFrequency = 1000000000;
+            Narrow2FrequencySpan = Transient_FrequencySpanEnum.R30;
+            Narrow2TimeSpan = 0.1;
+            Narrow2TimeOffset = 0;
+            Narrow2Reference = Transient_NarrowReferenceEnum.CENTer;
+            Narrow2VBW = 2000;
+            Narrow2VBWauto = true;
+
+        }
+
+        public override void Run()
+        {
+            SSAX.SetTransient_SweepType(Channel, SweepType);
+            SSAX.SetTransient_TimeCoupling(Channel, TimeCoupling);
+            SSAX.SetTransient_NumberOfPoints(Channel, NumberOfPoints);
+
+            if (SweepType == Transient_SweepTypeEnum.WN)
+            {
+                SSAX.SetTransient_FrequencyRange(Channel, FrequencyRange);
+                SSAX.SetTransient_WideTimeSpan(Channel, WideTimeSpan);
+                SSAX.SetTransient_WideTimeOffset(Channel, WideTimeOffset);
+                SSAX.SetTransient_WideReference(Channel, WideReference);
+                SSAX.SetTransient_WideVBW(Channel, WideVBW);
+                SSAX.SetTransient_WideVBWauto(Channel, WideVBWauto);
+            }
+
+            SSAX.SetTransient_CenterFrequency(Channel, 1, Narrow1CenterFrequency);
+            SSAX.SetTransient_FrequencySpan(Channel, 1, Narrow1FrequencySpan);
+            SSAX.SetTransient_NarrowTimeSpan(Channel, 1, Narrow1TimeSpan);
+            SSAX.SetTransient_NarrowTimeOffset(Channel, 1, Narrow1TimeOffset);
+            SSAX.SetTransient_NarrowReference(Channel, 1, Narrow1Reference);
+            SSAX.SetTransient_NarrowVBW(Channel, 1, Narrow1VBW);
+            SSAX.SetTransient_NarrowVBWauto(Channel, 1, Narrow1VBWauto);
+
+            if (SweepType == Transient_SweepTypeEnum.NN)
+            {
+                SSAX.SetTransient_CenterFrequency(Channel, 2, Narrow2CenterFrequency);
+                SSAX.SetTransient_FrequencySpan(Channel, 2, Narrow2FrequencySpan);
+                SSAX.SetTransient_NarrowTimeSpan(Channel, 2, Narrow2TimeSpan);
+                SSAX.SetTransient_NarrowTimeOffset(Channel, 2, Narrow2TimeOffset);
+                SSAX.SetTransient_NarrowReference(Channel, 2, Narrow2Reference);
+                SSAX.SetTransient_NarrowVBW(Channel, 2, Narrow2VBW);
+                SSAX.SetTransient_NarrowVBWauto(Channel, 2, Narrow2VBWauto);
+            }
+
+
+            UpgradeVerdict(Verdict.Pass);
+        }
+    }
+}
---
+++ Signal-Source-Analyzer/Instrument/SSAXTransient.cs
@@ -0,0 +1,304 @@
+﻿using OpenTap;
+using OpenTap.Plugins.PNAX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Signal_Source_Analyzer
+{
+    public enum Transient_SweepTypeEnum
+    {
+        [Scpi("WN")]
+        [Display("Wide-Narrow")]
+        WN,
+        [Scpi("NN")]
+        [Display("Narrow-Narrow")]
+        NN,
+    }
+
+    public enum Transient_FrequencyRangeEnum
+    {
+        [Scpi("R4")]
+        [Display("40 MHz – 160 MHz")]
+        R4,
+        [Scpi("R8")]
+        [Display("80 MHz – 320 MHz")]
+        R8,
+        [Scpi("R16")]
+        [Display("160 MHz – 640 MHz")]
+        R16,
+        [Scpi("R32")]
+        [Display("320 MHz – 1.28 GHz")]
+        R32,
+        [Scpi("R64")]
+        [Display("640 MHz – 2.56 GHz")]
+        R64,
+        [Scpi("R128")]
+        [Display("1.28 GHz – 5.12 GHz")]
+        R128,
+        [Scpi("R256")]
+        [Display("2.56 GHz – 8 GHz")]
+        R256,
+        [Scpi("R40M")]
+        [Display("1 MHz – 40 MHz")]
+        R40M,
+        [Scpi("R8G")]
+        [Display("256 MHz – 8 GHz")]
+        R8G,
+        [Scpi("RMAX")]
+        [Display("Max.Freq. > 8 GHz")]
+        RMAX,
+    }
+
+    public enum Transient_NarrowReferenceEnum
+    {
+        [Scpi("LEFT")]
+        [Display("Left")]
+        LEFT,
+        [Scpi("CENTer")]
+        [Display("Center")]
+        CENTer,
+        [Scpi("RIGHt")]
+        [Display("Right")]
+        RIGHt,
+    }
+
+    public enum Transient_WideReferenceEnum
+    {
+        [Scpi("LEFT")]
+        [Display("Left")]
+        LEFT,
+        [Scpi("CENTer")]
+        [Display("Center")]
+        CENTer,
+        [Scpi("RIGHt")]
+        [Display("Right")]
+        RIGHt,
+    }
+
+    public enum Transient_FrequencySpanEnum
+    {
+        [Scpi("R3K")]
+        [Display("3.125 kHz")]
+        R3K,
+        [Scpi("R25K")]
+        [Display("25 kHz")]
+        R25K,
+        [Scpi("R0_3")]
+        [Display("312.5 kHz")]
+        R0_3,
+        [Scpi("R2_5")]
+        [Display("2.5 MHz")]
+        R2_5,
+        [Scpi("R10")]
+        [Display("10 MHz")]
+        R10,
+        [Scpi("R30")]
+        [Display("30 MHz")]
+        R30,
+        [Scpi("R80")]
+        [Display("80 MHz")]
+        R80,
+        [Scpi("R160")]
+        [Display("160 MHz")]
+        R160,
+        [Scpi("R320")]
+        [Display("320 MHz")]
+        R320,
+    }
+
+
+
+
+    public partial class SSAX : PNAX
+    {
+        public void SetTransient_SweepType(int Channel, Transient_SweepTypeEnum SweepType)
+        {
+            string SweepTypeSCPI = Scpi.Format("{0}", SweepType);
+            ScpiCommand($"SENSe{Channel}:TR:STYPe  {SweepTypeSCPI}");
+        }
+
+        public Transient_SweepTypeEnum GetTransient_SweepType(int Channel)
+        {
+            return ScpiQuery<Transient_SweepTypeEnum>($"SENSe:TR:STYpe?");
+        }
+
+        public void SetTransient_TimeCoupling(int Channel, bool State)
+        {
+            string StateValue = State ? "1" : "0";
+            ScpiCommand($"SENSe{Channel}:TR:TIME:COUPling:STATe  {StateValue}");
+        }
+
+        public bool GetTransient_TimeCoupling(int Channel)
+        {
+            return ScpiQuery<bool>($"SENSe:TR:TIME:COUPling:STATe?");
+        }
+
+        public void SetTransient_NumberOfPoints(int Channel, int value)
+        {
+            ScpiCommand($"SENSe{Channel}:SWEep:POINts {value}");
+        }
+
+        public int GetTransient_NumberOfPoints(int Channel)
+        {
+            return ScpiQuery<int>($"SENSe{Channel}:SWEep:POINts?");
+        }
+
+        public void SetTransient_FrequencyRange(int Channel, Transient_FrequencyRangeEnum FrequencyRange)
+        {
+            string FrequencyRangeSCPI = Scpi.Format("{0}", FrequencyRange);
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:FREQuency:RANGe  {FrequencyRangeSCPI}");
+        }
+
+        public Transient_FrequencyRangeEnum GetTransient_FrequencyRange(int Channel)
+        {
+            return ScpiQuery<Transient_FrequencyRangeEnum>($"SENSe:TR:WIDE:FREQuency:RANGe?");
+        }
+
+        public void SetTransient_MaxFrequency(int Channel, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:FREQuency:MAXimum  {value}");
+        }
+
+        public double GetTransient_MaxFrequency(int Channel)
+        {
+            return ScpiQuery<double>($"SENSe:TR:WIDE:FREQuency:MAXimum?");
+        }
+
+        public double GetTransient_MinFrequency(int Channel)
+        {
+            return ScpiQuery<double>($"SENSe:TR:WIDE:FREQuency:MINimum?");
+        }
+
+        public void SetTransient_WideTimeSpan(int Channel, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:TIME:SPAN  {value}");
+        }
+
+        public double GetTransient_WideTimeSpan(int Channel)
+        {
+            return ScpiQuery<double>($"SENSe:TR:WIDE:TIME:SPAN?");
+        }
+
+        public void SetTransient_WideTimeOffset(int Channel, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:TIME:OFFSet  {value}");
+        }
+
+        public double GetTransient_WideTimeOffset(int Channel)
+        {
+            return ScpiQuery<double>($"SENSe:TR:WIDE:TIME:OFFSet?");
+        }
+
+        public void SetTransient_WideReference(int Channel, Transient_WideReferenceEnum WideReference)
+        {
+            string WideReferenceSCPI = Scpi.Format("{0}", WideReference);
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:TIME:REFerence  {WideReferenceSCPI}");
+        }
+
+        public Transient_WideReferenceEnum GetTransient_WideReference(int Channel)
+        {
+            return ScpiQuery<Transient_WideReferenceEnum>($"SENSe:TR:WIDE:TIME:REFerence?");
+        }
+
+        public void SetTransient_WideVBW(int Channel, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:BANDwidth:VIDeo {value}");
+        }
+
+        public double GetTransient_WideVBW(int Channel)
+        {
+            return ScpiQuery<double>($"SENSe:TR:WIDE:BANDwidth:VIDeo?");
+        }
+
+        public void SetTransient_WideVBWauto(int Channel, bool State)
+        {
+            string StateValue = State ? "1" : "0";
+            ScpiCommand($"SENSe{Channel}:TR:WIDE:BANDwidth:VIDeo:AUTO {StateValue}");
+        }
+
+        public bool GetTransient_WideVBWauto(int Channel)
+        {
+            return ScpiQuery<bool>($"SENSe:TR:WIDE:BANDwidth:VIDeo:AUTO?");
+        }
+
+        public void SetTransient_CenterFrequency(int Channel, int band, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:FREQuency:CENTer  {value}");
+        }
+
+        public double GetTransient_CenterFrequency(int Channel, int band)
+        {
+            return ScpiQuery<double>($"SENSe:TR:NARRow{band}:FREQuency:CENTer?");
+        }
+
+        public void SetTransient_FrequencySpan(int Channel, int band, Transient_FrequencySpanEnum FrequencySpan)
+        {
+            string FrequencySpanSCPI = Scpi.Format("{0}", FrequencySpan);
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:FREQuency:RANGe  {FrequencySpanSCPI}");
+        }
+
+        public Transient_FrequencySpanEnum GetTransient_FrequencySpan(int Channel, int band)
+        {
+            return ScpiQuery<Transient_FrequencySpanEnum>($"SENSe:TR:NARRow{band}:FREQuency:RANGe?");
+        }
+
+        public void SetTransient_NarrowTimeSpan(int Channel, int band, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:TIME:SPAN  {value}");
+        }
+
+        public double GetTransient_NarrowTimeSpan(int Channel, int band)
+        {
+            return ScpiQuery<double>($"SENSe:TR:NARRow{band}:TIME:SPAN?");
+        }
+
+        public void SetTransient_NarrowTimeOffset(int Channel, int band, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:TIME:OFFSet  {value}");
+        }
+
+        public double GetTransient_NarrowTimeOffset(int Channel, int band)
+        {
+            return ScpiQuery<double>($"SENSe:TR:NARRow{band}:TIME:OFFSet?");
+        }
+
+        public void SetTransient_NarrowReference(int Channel, int band, Transient_NarrowReferenceEnum NarrowReference)
+        {
+            string NarrowReferenceSCPI = Scpi.Format("{0}", NarrowReference);
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:TIME:REFerence  {NarrowReferenceSCPI}");
+        }
+
+        public Transient_NarrowReferenceEnum GetTransient_NarrowReference(int Channel, int band)
+        {
+            return ScpiQuery<Transient_NarrowReferenceEnum>($"SENSe:TR:NARRow{band}:TIME:REFerence?");
+        }
+
+        public void SetTransient_NarrowVBW(int Channel, int band, double value)
+        {
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:BANDwidth:VIDeo:VALUe {value}");
+        }
+
+        public double GetTransient_NarrowVBW(int Channel, int band)
+        {
+            return ScpiQuery<double>($"SENSe:TR:NARRow{band}:BANDwidth:VIDeo:VALUe?");
+        }
+
+        public void SetTransient_NarrowVBWauto(int Channel, int band, bool State)
+        {
+            string StateValue = State ? "1" : "0";
+            ScpiCommand($"SENSe{Channel}:TR:NARRow{band}:BANDwidth:VIDeo:AUTO {StateValue}");
+        }
+
+        public bool GetTransient_NarrowVBWauto(int Channel, int band)
+        {
+            return ScpiQuery<bool>($"SENSe:TR:NARRow{band}:BANDwidth:VIDeo:AUTO?");
+        }
+
+
+
+    }
+}
